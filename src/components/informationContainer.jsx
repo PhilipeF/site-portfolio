b0254564384b2bc4c644746727e05e730e270aa7
rick <philipesferreiraa@gmail.com>
2023-01-03 17:14:12 -0300

    componente sidebar ok

--- src/components/informationContainer.jsx
+++ src/components/informationContainer.jsx
@@ -1,9 +1,34 @@
 import '../styles/components/informationcontainer.sass'
-import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai';
+import { AiFillPhone, AiFillEnvironment, AiOutlineMail, AiFillAlert } from 'react-icons/ai';
 
 const InformationContainer = () => {
     return (
-        <p>Teste</p>
+        <section id="information">
+            <div className="info-card">
+                <AiFillPhone id="phone-icon" />
+                <div>
+                    <h3>Telefone</h3>
+                    <p>(24)999917-3920</p>
+                </div>
+            </div>
+
+            <div className="info-card">
+                <AiOutlineMail id="email-icon" />
+                <div>
+                    <h3>E-mail</h3>
+                    <p>philipesferreiraa@gmail.com</p>
+                </div>
+            </div>
+
+            <div className="info-card">
+                <AiFillEnvironment id="pin-icon" />
+                <div>
+                    <h3>Localização</h3>
+                    <p>Volta Redonda/RJ</p>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
