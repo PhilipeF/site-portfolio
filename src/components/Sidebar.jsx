35ff4489f47011426529b90e84a37eb324ca37e8
rick <philipesferreiraa@gmail.com>
2023-01-03 00:00:00 -0300

    criando component socialnetworking

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import '../styles/components/sidebar.sass';
 import profile from '../img/profile.jpeg';
+import SoicialNetworking from './SocialNetWorking';
 
 const Sidebar = () => {
     return (
         <aside id="sidebar">
             <img src={profile} alt="" />
-            <p className='title'>Desenvolvedor</p>
-            <p>redes sociais</p>
+            <SoicialNetworking/>
+            <p className="title">Desenvolvedor</p>
             <p>informações de contato</p>
             <a href="" className="btn">
                 Download Currículo
---
+++ src/components/SocialNetWorking.jsx
@@ -0,0 +1,10 @@
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import '../styles/components/socialnetworking.sass';
+
+const SoicialNetworking = () => {
+    return (
+        <div> <FaLinkedin /> <FaGithub /> <FaInstagram /> </div>
+    )
+}
+
+export default SoicialNetworking
