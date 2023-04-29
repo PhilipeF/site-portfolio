3365574277536e73d86849622fe78c2285789e69
rick <philipesferreiraa@gmail.com>
2023-04-29 15:30:25 -0300

    upgrade about

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -7,11 +7,11 @@ const AboutContainer = () => {
             <p>
                 Apaixonado por tecnologia e por desenvolvimento web procuro aprender e
                 desenvolver sistemas utilizando o que tem de mais moderno na área hoje em dia como,
-                HTML, CSS, JavaScript, TypeScript, ReactJS, React Native e NodeJS.
+                HTML, CSS, Javascript, Typescript, ReactJS, React Native e NodeJS.
             </p>
             <p>
                 O meu foco desde 2020 é desenvolvimento web front end utilizando a biblioteca ReactJs,
-                mas ja desenvolvi sistemas mobile utilizando React Native e em outros casos tive contato com o back end da aplicação utilizando NodeJS.
+                mas ja desenvolvi sistemas mobile utilizando React Native e em outros casos tive contato com o back end da aplicação utilizando NodeJS, mySQL, MongoDB e Prisma.
             </p>
         </section>
     )
