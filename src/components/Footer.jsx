72087335dfa4b66aa11cbf7892795437ac6c204d
rick <philipesferreiraa@gmail.com>
2023-09-06 11:05:45 -0300

    Feat: Adicionando o footer na pagina

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../styles/components/footer.sass';
+
+const Footer = () => {
+  return (
+    <p className="footer">© 2023 Philipe Ferreira - Todos os direitos reservados</p>
+  )
+}
+
+export default Footer
