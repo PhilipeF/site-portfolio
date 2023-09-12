e80c697450cc55eb417da2eb1cc876efe658c34e
rick <philipesferreiraa@gmail.com>
2023-09-12 09:04:47 -0300

    fix: alinhanmento dos textos

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
     return (
         <section id="about-container">
             <h2>Sobre</h2>
-            <p>
+            <p className='about'>
                 Olá, me chamo Philipe sou natural de Volta Redonda - RJ e atualmente estou me especializando em javascript, typescript, react.js e next.js. Já passei por cursos, treinamentos e bootcamps com o Gustavo Guanabara, Maky Brito, Diego Fernandes, Roger Melo, Matheus Battisti, Bonieki Lacerda e entre outros professores, a fim de sempre estar aprimorando os meus conhecimentos e me manter atualizado. Sou fascinado pela arte de aprender, gosto de estudar desenvolvimento web e de dar vida ao layout a fim de proporcionar uma experiência incrível ao cliente final.
                 Como experiência tenho 3 (três) anos como “desenvolvedor web freelancer” ativo e atuante em plataformas como a 99Freelas, Workanda, Freelancer, Upwork e entre outras desenvolvendo soluções para "e-commerces" utilizando como principais skills o WordPress, VTEX IO, Javascript, React.js, HTML e CSS. Em VTEX IO pude tirar uma certificação recentemente.
             </p>
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -64,7 +64,7 @@ const TechnologiesContainer = () => {
                         {tech.icon}
                         <div className="technology-info">
                             <h3>{tech.name}</h3>
-                            <p>{tech.description}</p>
+                            <p className='tech'>{tech.description}</p>
                         </div>
                     </div>
                 ))}
