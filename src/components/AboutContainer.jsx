f0a90f94b4b21e8d7b85c01d947fe7050f1c0683
PhilipeF <philipesferreiraa@gmail.com>
2024-01-07 13:43:56 -0300

    Fix: about

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,11 +4,19 @@ const AboutContainer = () => {
     return (
         <section id="about-container">
             <h2>Sobre</h2>
-            <p className='about'>
-
-                "Olá, meu nome é Philipe, nasci em Volta Redonda - RJ e estou atualmente focado em aprimorar minhas habilidades em JavaScript, TypeScript, React.js e Next.js. Ao longo da minha jornada, participei de cursos, treinamentos e bootcamps ministrados por renomados profissionais, incluindo Gustavo Guanabara, Maky Brito, Diego Fernandes, Roger Melo, Matheus Battisti, Bonieki Lacerda, e outros professores, buscando constantemente atualizações e conhecimentos.
-
-                Sou apaixonado pela arte de aprender e dedicado ao estudo do desenvolvimento web, especialmente em transformar designs em experiências incríveis para os clientes. Com uma trajetória de 3 anos como desenvolvedor web freelancer ativo, tive a oportunidade de colaborar em diversas plataformas como 99Freelas, Workanda, Freelancer, Upwork, entre outras. Durante esse período, concentrei meus esforços no desenvolvimento de soluções para e-commerces, utilizando minhas principais habilidades em WordPress, VTEX IO, JavaScript, React.js, HTML e CSS. Recentemente, obtive uma certificação em JavaScript, React.js e VTEX IO, consolidando ainda mais meu conhecimento nessa área."
+            <p>
+                Olá, meu nome é Philipe, nasci em Volta Redonda - RJ e estou atualmente focado em aprimorar minhas habilidades em JavaScript, TypeScript, React.js e Next.js.
+            </p>
+            <p>
+                Ao longo da minha jornada, participei de cursos, treinamentos e bootcamps ministrados por renomados profissionais, incluindo Gustavo Guanabara, Maky Brito, Diego Fernandes, Roger Melo, Matheus Battisti, Bonieki Lacerda, e outros professores, buscando constantemente atualizações e conhecimentos.
+            </p>
+            <p> Sou apaixonado pela arte de aprender e dedicado ao estudo do desenvolvimento web, especialmente em criar novas funcionalidades e transformar designs em experiências incríveis para os clientes finais.
+            </p>
+            <p>
+                Com uma trajetória de 3 (três) anos como desenvolvedor web freelancer ativo, tive a oportunidade de colaborar em diversas plataformas como 99Freelas, Workana, Freelancer, Upwork, entre outras. Durante esse período, concentrei meus esforços no desenvolvimento de soluções para e-commerce, utilizando minhas principais habilidades em WordPress, VTEX IO, JavaScript, React.js, HTML e CSS. Recentemente, obtive certificação em JavaScript, React.js e VTEX IO, consolidando ainda mais meu conhecimento nessa área.
+            </p>
+            <p>
+                O meu foco é me especializar no front-end, mas já tive a oportunidade de trabalhar com o back-end de algumas aplicações utilizando o Node.js e MySQL.
             </p>
         </section>
     )
