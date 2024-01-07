cbaa562ea7c8a16a14f9bc4fa201c5f7bf1d3410
PhilipeF <philipesferreiraa@gmail.com>
2024-01-07 12:56:32 -0300

    Feat: Upgrade copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/components/footer.sass';
 
 const Footer = () => {
   return (
-    <p className="footer">© 2023 Philipe Ferreira - Todos os direitos reservados</p>
+    <p className="footer">© 2024 Philipe Ferreira - Todos os direitos reservados</p>
   )
 }
 
