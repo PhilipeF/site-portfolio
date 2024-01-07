3035fbd82c11d9b14d65682a302a228c4fd17fe0
PhilipeF <philipesferreiraa@gmail.com>
2024-01-07 13:02:46 -0300

    Feat: about

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,8 +5,10 @@ const AboutContainer = () => {
         <section id="about-container">
             <h2>Sobre</h2>
             <p className='about'>
-                Olá, me chamo Philipe, sou natural de Volta Redonda - RJ e atualmente estou me especializando em javascript, typescript, react.js e next.js. Já passei por cursos, treinamentos e bootcamps com o Gustavo Guanabara, Maky Brito, Diego Fernandes, Roger Melo, Matheus Battisti, Bonieki Lacerda e entre outros professores, a fim de sempre estar aprimorando os meus conhecimentos e me manter atualizado. Sou fascinado pela arte de aprender, gosto de estudar desenvolvimento web e de dar vida ao layout a fim de proporcionar uma experiência incrível ao cliente final.
-                Como experiência tenho 3 (três) anos como “desenvolvedor web freelancer” ativo e atuante em plataformas como a 99Freelas, Workanda, Freelancer, Upwork e entre outras desenvolvendo soluções para "e-commerces" utilizando como principais skills o WordPress, VTEX IO, Javascript, React.js, HTML e CSS. Em VTEX IO pude tirar uma certificação recentemente.
+
+                "Olá, meu nome é Philipe, nasci em Volta Redonda - RJ e estou atualmente focado em aprimorar minhas habilidades em JavaScript, TypeScript, React.js e Next.js. Ao longo da minha jornada, participei de cursos, treinamentos e bootcamps ministrados por renomados profissionais, incluindo Gustavo Guanabara, Maky Brito, Diego Fernandes, Roger Melo, Matheus Battisti, Bonieki Lacerda, e outros professores, buscando constantemente atualizações e conhecimentos.
+
+                Sou apaixonado pela arte de aprender e dedicado ao estudo do desenvolvimento web, especialmente em transformar designs em experiências incríveis para os clientes. Com uma trajetória de 3 anos como desenvolvedor web freelancer ativo, tive a oportunidade de colaborar em diversas plataformas como 99Freelas, Workanda, Freelancer, Upwork, entre outras. Durante esse período, concentrei meus esforços no desenvolvimento de soluções para e-commerces, utilizando minhas principais habilidades em WordPress, VTEX IO, JavaScript, React.js, HTML e CSS. Recentemente, obtive uma certificação em React.js, JavaScript e VTEX IO, consolidando ainda mais meu conhecimento nessa área."
             </p>
         </section>
     )
